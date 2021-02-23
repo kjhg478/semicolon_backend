0851b5441fa2cd7471c6a6c020851e6a9df87871
JHK <31474272+kjhg478@users.noreply.github.com>
2021-02-23 10:18:20 +0900

    My Server

--- src/recommendation.js
+++ src/recommendation.js
@@ -3,7 +3,7 @@ const recommendKey = process.env.RECOMMEND_KEY;
 
 export const getRecommendation = async () => { 
         try {
-            const { data } = await axios.get(`http://218.156.65.175:5000/recommendation/key=${recommendKey}`);
+            const { data } = await axios.get(`http://211.41.119.208:5000/recommendation/key=${recommendKey}`);
             return data;
         } catch (e) { 
             console.log("데이터가 존재 하지 않습니다.");
